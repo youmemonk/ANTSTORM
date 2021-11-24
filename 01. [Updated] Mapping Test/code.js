ef09e5db1320ddae589e7d97fc260416d2d1949e
youmemonk <ukeym160@gmail.com>
2021-11-24 16:09:48 +0530

    Data Fetching with new Logic Completed

--- 01. [Updated] Mapping Test/code.js
+++ 01. [Updated] Mapping Test/code.js
@@ -41,3 +41,47 @@ function arrayToJSONObject(fileList) {
   }
   return formatted;
 }
+
+function fetchData(id) {
+  // var sheetId = "1Ais-7oPMCR_C2rNJgGsiPIeXXejXMazy0gMbLP9DFXg";
+  // var sheetId = "1Ais-7oPMCR_C2rNJgGsiPIeXXejXMazy0gMbLP9DFXg";
+  var sheetId = id;
+  var book = SpreadsheetApp.openById(sheetId);
+
+  //! Get Sheet Name
+  var list = [];
+  for (var i = 0; i < book.getSheets().length; i++) {
+    list.push(book.getSheets()[i].getName());
+  }
+  //! Hardcoded Sheetname here
+  var sheetName = list[0];
+
+  var sheet = book.getSheetByName(sheetName);
+  var values = sheet.getDataRange().getValues();
+
+  //! Get Urls
+  var urlList = ["Document Link"];
+  for (var i = 1; i < values.length; i++) {
+    var url = sheet
+      .getRange(`B${i + 1}`)
+      .getRichTextValue()
+      .getLinkUrl();
+    urlList.push(url);
+  }
+
+  var fileList = [];
+  //! Merge urls and Sheet Data into Filelist
+  for (var i = 0; i < values.length; i++) {
+    fileList.push(values[i].concat(urlList[i]));
+  }
+
+  //! Filelist to JSON object
+  // var startTime = (new Date()).getTime()
+  var data = arrayToJSONObject(fileList);
+  Logger.log(data);
+  // var endTime = (new Date()).getTime()
+  // Logger.log(`Call to Arraywala took ${endTime - startTime} milliseconds`)
+
+  //! Data has sheet data + url links
+  return data;
+}
